290120c5432ce0e14bde6537ae717c0ddae2cc31
Ricardo González <76540701+sxgbln@users.noreply.github.com>
2024-06-25 20:27:50 -0600

    Update acciones.js

--- acciones.js
+++ acciones.js
@@ -1,7 +1,4 @@
-
-
-
-//FUNCION PARA BUSCADOR
+// FUNCION PARA BUSCADOR
 
 function normalizeText(text) {
   return text.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
@@ -12,6 +9,7 @@ function filterCards() {
   const normalizedSearchInput = normalizeText(searchInput);
   const cardGroup = document.getElementById('cardGroup');
   const cards = cardGroup.getElementsByClassName('col');
+  let firstVisibleCard = null;
 
   for (let i = 0; i < cards.length; i++) {
     const card = cards[i];
@@ -20,8 +18,26 @@ function filterCards() {
 
     if (normalizedTitle.includes(normalizedSearchInput)) {
       card.style.display = 'block';
+      if (!firstVisibleCard) {
+        firstVisibleCard = card;
+      }
     } else {
       card.style.display = 'none';
     }
   }
+
+  if (firstVisibleCard) {
+    firstVisibleCard.scrollIntoView({ behavior: 'smooth' });
+  }
 }
+
+function resetSearch() {
+  const cardGroup = document.getElementById('cardGroup');
+  const cards = cardGroup.getElementsByClassName('col');
+
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].style.display = 'block';
+  }
+}
+
+document.getElementById('searchButton').addEventListener('click', filterCards);
